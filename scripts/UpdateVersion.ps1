3812f7aecc4f6f00f1efb2c7d4371261be1400ee
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2025-08-07 07:50:17 +0800

    Refactor version update step in Azure Pipelines to use external script for better maintainability

---
+++ scripts/UpdateVersion.ps1
@@ -0,0 +1,14 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true, HelpMessage = "Path to the package.json file to update.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$PackageJsonPath,
+
+    [Parameter(Mandatory = $true, HelpMessage = "Version string to update in package.json.")]
+    [ValidateNotNullOrEmpty()]
+    [string]$Version
+)
+
+$packageJson = Get-Content -Path $PackageJsonPath -Raw | ConvertFrom-Json
+$packageJson.version = $Version
+$packageJson | ConvertTo-Json -Depth 10 | Set-Content -Path $PackageJsonPath
